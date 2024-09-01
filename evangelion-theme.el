1255e20763c2d864f68d42493bddedb7ff9e0899
Drew Jose <mail@drewsh.com>
2024-09-01 16:34:28 +0530

    CI and initialise with copyright

---
+++ evangelion-theme.el
@@ -0,0 +1,11 @@
+;;; evangelion-theme.el --- a purple colorscheme inspired by neon genesis evangelion -*- lexical-binding: t; -*-
+;;; Version: 0.01
+
+;; Copyright (C) 2024 Andrew Jose, xero harrison
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Author: Andrew Jose <mail@drewsh.com>
+;; Maintainer: Andrew Jose <mail@drewsh.com>
+;; URL: https://github.com/crmsnbleyd/evangelion-theme
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: faces, theme
