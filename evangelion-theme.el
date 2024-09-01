abdb5648b88dc7e0d0d3eaba2401a6b70855a86c
Drew Jose <mail@drewsh.com>
2024-09-01 19:19:37 +0530

    add commentary

--- evangelion-theme.el
+++ evangelion-theme.el
@@ -1,4 +1,4 @@
-;;; evangelion-theme.el --- a purple colour scheme inspired by neon genesis evangelion -*- lexical-binding: t; -*-
+;;; evangelion-theme.el --- A dark colour scheme inspired by Neon Genesis Evangelion -*- lexical-binding: t; -*-
 ;;; Version: 0.01
 
 ;; Copyright (C) 2024 Andrew Jose, xero harrison
@@ -9,47 +9,57 @@
 ;; URL: https://github.com/crmsnbleyd/evangelion-theme
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;;
+;; evangelion is a dark color scheme that pays homage to the anime
+;; "Neon Genesis Evangelion".
+
+;;; Code:
 
 (deftheme evangelion
   "Purple colour scheme inspired by neon genesis evangelion."
   :kind 'color-scheme
   :background-mode 'dark)
 
-(defvar evangelion-bg       "#201430")
-(defvar evangelion-fg       "#E1D6F8")
-(defvar evangelion-green    "#87FF5F")
-(defvar evangelion-green2   "#9CDA7C")
-(defvar evangelion-hl       "#39274D")
-(defvar evangelion-fade     "#A1A0AD")
-(defvar evangelion-lavender "#AB92FC")
-(defvar evangelion-purple   "#B968FC")
-(defvar evangelion-orange   "#E6BB85")
-(defvar evangelion-orange2  "#D99145")
+(let*
+    ((class '((class color) (min-colors 256)))
+     (evangelion-bg       "#201430")
+     (evangelion-fg       "#E1D6F8")
+     (evangelion-green    "#87FF5F")
+     (evangelion-green2   "#9CDA7C")
+     (evangelion-hl       "#39274D")
+     (evangelion-fade     "#A1A0AD")
+     (evangelion-lavender "#AB92FC")
+     (evangelion-purple   "#B968FC")
+     (evangelion-orange   "#E6BB85")
+     (evangelion-orange2  "#D99145"))
 
-
-(custom-theme-set-faces
-   'evangelion
-     `(default ((t (:background ,evangelion-bg :foreground ,evangelion-fg))))
+     
+     (custom-theme-set-faces
+      'evangelion
+      `(default ((t (:background ,evangelion-bg :foreground ,evangelion-fg))))
      ;;; font lock default faces
-     `(font-lock-builtin-face ((t (:foreground ,evangelion-green2))))
-     `(font-lock-comment-face
-       ((t (:foreground ,evangelion-fade :background ,evangelion-hl))))
-     `(font-lock-constant-face ((t (:foreground ,evangelion-orange2))))
-     `(font-lock-doc-string-face ((t (:foreground ,evangelion-lavender))))
-     `(font-lock-function-name-face ((t (:foreground ,evangelion-green2))))
-     `(font-lock-keyword-face ((t (:bold t :foreground ,evangelion-green))))
-     `(font-lock-preprocessor-face ((t (:foreground ,evangelion-green))))
-     `(font-lock-reference-face ((t (:foreground ,evangelion-purple))))
-     `(font-lock-string-face ((t (:foreground ,evangelion-lavender))))
-     `(font-lock-type-face ((t (:foreground ,evangelion-green2))))
-     `(font-lock-variable-name-face ((t (:foreground ,evangelion-purple))))
-     `(font-lock-warning-face ((t (:bold t :foreground ,evangelion-orange))))
+      `(font-lock-builtin-face ((t (:foreground ,evangelion-green2))))
+      `(font-lock-comment-face
+	((t (:foreground ,evangelion-fade :background ,evangelion-hl))))
+      `(font-lock-constant-face ((t (:foreground ,evangelion-orange2))))
+      `(font-lock-doc-string-face ((t (:foreground ,evangelion-lavender))))
+      `(font-lock-function-name-face ((t (:foreground ,evangelion-green2))))
+      `(font-lock-keyword-face ((t (:bold t :foreground ,evangelion-green))))
+      `(font-lock-preprocessor-face ((t (:foreground ,evangelion-green))))
+      `(font-lock-reference-face ((t (:foreground ,evangelion-purple))))
+      `(font-lock-string-face ((t (:foreground ,evangelion-lavender))))
+      `(font-lock-type-face ((t (:foreground ,evangelion-green2))))
+      `(font-lock-variable-name-face ((t (:foreground ,evangelion-purple))))
+      `(font-lock-warning-face ((t (:bold t :foreground ,evangelion-orange))))
      ;;; paren
-     `(show-paren-match
-       ((t (:foreground ,evangelion-purple :background ,evangelion-hl :bold t))))
+      `(show-paren-match
+	((t (:foreground ,evangelion-purple :background ,evangelion-hl :bold t))))
      ;;; line number
-    `(line-number-current-line
-       ((t (:inherit line-number :foreground ,evangelion-green)))))
+      `(line-number-current-line
+	((t (:inherit line-number :foreground ,evangelion-green))))))
 
 (and load-file-name
      (boundp 'custom-theme-load-path)
@@ -58,6 +68,6 @@
                    (file-name-directory load-file-name))))
 
 (provide-theme 'evangelion)
-(provide 'evangelion-theme)
+(provide 'evangelion)
 
 ;;; evangelion-theme.el ends here
